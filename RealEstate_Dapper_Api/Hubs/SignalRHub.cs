0a8028cab33ccfab26f4e4f535b6ea2b10d78898
Abdurrahman Hac Said <abdurahman.hajsaid.98@gmail.com>
2025-02-13 17:15:10 +0300

    Ders 54 4:00 13.02.2025 17:15

--- RealEstate_Dapper_Api/Hubs/SignalRHub.cs
+++ RealEstate_Dapper_Api/Hubs/SignalRHub.cs
@@ -4,5 +4,18 @@ namespace RealEstate_Dapper_Api.Hubs
 {
     public class SignalRHub : Hub
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SignalRHub(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task SendCategoryCount()
+        {
+            var clientCategoryCount = _httpClientFactory.CreateClient();
+            var responseMessageCategoryCount = await clientCategoryCount.GetAsync("http://localhost:5092/api/Statistic/CategoryCount");
+            var jsonDataCategoryCount = await responseMessageCategoryCount.Content.ReadAsStringAsync();
+            await Clients.All.SendAsync("ReceiveCategoryCount", jsonDataCategoryCount);
+        }
     }
 }
--- RealEstate_Dapper_UI/obj/Debug/net8.0/RealEstate_Dapper_UI.AssemblyInfo.cs
+++ RealEstate_Dapper_UI/obj/Debug/net8.0/RealEstate_Dapper_UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate_Dapper_UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d6f67dc6ab7d5447a86853421beed5cecfe7e80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7832725fd1c2dcf92159cf9976f0f95402403f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate_Dapper_UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate_Dapper_UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

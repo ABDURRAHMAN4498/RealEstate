3c10574da5093c34f90bf7522105b18cd91b7a1e
Abdurrahman <abdulrahman.hajsaid.98@gmail.com>
2025-03-24 02:54:19 +0300

    Ders 77 24.03.2025 02:54

---
+++ RealEstate_Dapper_UI/Dtos/ProductDetailDtos/GetProductDetailByIdDto.cs
@@ -0,0 +1,6 @@
+namespace RealEstate_Dapper_UI.Dtos.ProductDetailDtos;
+
+public class GetProductDetailByIdDto
+{
+    
+}
---
+++ RealEstate_Dapper_UI/Dtos/ProductImageDto/GetProductImageDto.cs
@@ -0,0 +1,6 @@
+namespace RealEstate_Dapper_UI.Dtos.ProductImageDto;
+
+public class GetProductImageDto
+{
+    
+}
